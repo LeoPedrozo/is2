48dc18dad409c4e7378a33be3a75ba02c8933825
LeoPedrozo <oscarleonardopedrozo@gmail.com>
2021-08-08 15:48:11 -0400

    Prueba de documentacion

--- holamundo.py
+++ holamundo.py
@@ -1,1 +1,3 @@
+"""" Hola mundo de bienvenida del repositorio recien creado"""
+
 print("Hola bienvenido al proyecto del grupo 8 is2 - 2021")
--- is2/views.py
+++ is2/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 from django.template import  Template,Context
 from django.shortcuts import render
 def saludo(request):
-    return render(request,"menuprincipal.html",{"nombre":"Leo"})
+    return render(request,"menuprincipal.html",{"n:ombre":"Leo"})
 

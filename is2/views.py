e405ff91166ce1f84e2f24bf2e9b667df2ced721
josee <jose.elias.gv99@gmail.com>
2021-08-16 13:45:56 -0400

    Se agrego codigo que hace que el avatar del usuario se muestre en sidenav

--- is2/views.py
+++ is2/views.py
@@ -3,15 +3,20 @@ from django.db import models
 
 from django.template import  Template,Context
 from django.shortcuts import render
+from allauth.socialaccount.models import SocialAccount
+
+
 
 #Hola mundo para probar django
 def saludo(request):
     return render(request, "holaMundo.html", {"nombre": "Jose"})
 
 def inicio(request):
-    return render(request, "sidenav.html", {})
+    fotodeususario= SocialAccount.objects.filter(user=request.user)[0].extra_data['picture']
+    return render(request, "sidenav.html", {"avatar":fotodeususario})
 
 #Para acceder directamente a los archivos guardados en el directorio docs
 #(Todavia no se ha implementado)
 def documentaciones(request):
     return render(request,"html/index.html",{})
+

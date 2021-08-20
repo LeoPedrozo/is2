697f38db75c9edac419fd0b64c286970d445445d
Luis <luis17.alberto.baez@gmail.com>
2021-08-20 14:55:56 -0400

    form + crearRol.html

---
+++ GestionPermisos/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+class crearRolForm(forms.Form):
+    Rol = forms.CharField()
+    OPTIONS = (
+        ("add", "Agregar"),
+        ("delete", "Borrar"),
+        ("change","Modificar"),
+        ("view","Ver"),
+    )
+    Historia = forms.MultipleChoiceField(required=False,widget=forms.CheckboxSelectMultiple, choices=OPTIONS)
+    Proyecto = forms.MultipleChoiceField(required=False,widget=forms.CheckboxSelectMultiple, choices=OPTIONS)
+    Sprint = forms.MultipleChoiceField(required=False, widget=forms.CheckboxSelectMultiple, choices=OPTIONS)
+
+
+class asignarRolForm(forms.Form):
+    nombreUsuario = forms.CharField(30)
+    #roles = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,choices = )
